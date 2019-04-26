ee69d0bd7ba1db1f4d22d436dc077f4f0d2a204d
Jonathan Ruiz <jonathan.ruiz@gateb.com>
2019-04-26 18:30:47 +0200

    create day 3 solution

---
+++ oop2_day3/oop2_day3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop2_day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
